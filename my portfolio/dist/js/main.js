9ab5eadcec77d1156e023769ec9119add5d3db7e
praiseolotu <olioxxayo@gmail.com>
2021-07-16 12:01:08 +0100

    updated files

--- my portfolio/dist/js/main.js
+++ my portfolio/dist/js/main.js
@@ -2,24 +2,23 @@ const menuButton = document.querySelector(".menu-button");
 const hamburger = document.querySelector(".menu-button__burger");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
-const navItems = document.querySelectorAll(".menu-nav__items");  
+const navItems = document.querySelectorAll(".menu-nav__items");
 let showMenu = false;
 
-menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    if (!showMenu) {
-        hamburger.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItems.forEach(items => items.classList.add('open'));
-        showMenu = true;
-    }
-    else{
-        hamburger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        navItems.forEach(items => items.classList.remove('open'));
-        showMenu = false;
-    }
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItems.forEach((items) => items.classList.add("open"));
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItems.forEach((items) => items.classList.remove("open"));
+    showMenu = false;
+  }
 }
